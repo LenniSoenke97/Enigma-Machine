87ff8412ceafd575d03d7c7e39b2671b7c25cf6c
Leonhard Soenke <lhs19@graphic12.doc.ic.ac.uk>
2019-11-06 19:15:50 +0000

    Refactor, checking if it still works

--- enigma.cpp
+++ enigma.cpp
@@ -4,7 +4,7 @@
 #include<iostream>
 
 /*
-Plugboard
+  Plugboard
 */
 void Plugboard::convert(char* input_char) {
   int input_int = static_cast<int>(*input_char) - 65;
@@ -19,7 +19,7 @@ void Plugboard::convert(char* input_char) {
 }
 
 /*
-Rotor
+  Rotor
 */
 void Rotor::remap() {
   int  mapped_input;
@@ -59,16 +59,17 @@ void Rotor::rotate(int by_positions) {
       new_config_file_index += 26;
     }
     new_config_file_offsets[new_config_file_index] = config_file_offsets[old_config_file_index];
-    }
+  }
 
   for (int index=0; index < 26; index++) {
     config_file_offsets[index] = new_config_file_offsets[index];
-    }
+  }
+  this->remap();
 };
 
 
 /*
-Reflector
+  Reflector
 */
 void Reflector::convert(char* input_char) {
   int input_int = static_cast<int>(*input_char) - 65;
@@ -83,26 +84,30 @@ void Reflector::convert(char* input_char) {
 }
 
 /*
-EnigmaMachine
+  EnigmaMachine
 */
 void EnigmaMachine::rotateRotors() {
-   int rotors_to_rotate = 1;
+  int rotors_to_rotate = 1;
   for(; rotors_to_rotate <= number_of_rotors; rotors_to_rotate++) {
     if(!((this->rotors[number_of_rotors-rotors_to_rotate])->at_rotation_notch())) break;
   }
   for(int current_rotor = 0; current_rotor < rotors_to_rotate && current_rotor < number_of_rotors; current_rotor++) {
     (this->rotors[number_of_rotors-current_rotor-1])->rotate();
+
+  }
+  /*for(int current_rotor = 0; current_rotor < number_of_rotors; current_rotor++) {
     (this->rotors[current_rotor])->remap();
-    }
+  }*/
 }
+
 void EnigmaMachine::convert(char* input_char) {
   this->rotateRotors();
+     
   // Plugboard convert  
   plugboard->convert(input_char);
 
   // Rotor convert
   for(int current_rotor = (number_of_rotors-1); 0 <= current_rotor; current_rotor--) {
-    
     (this->rotors[current_rotor])->convert_forward(input_char);
   }
   
--- enigma.hpp
+++ enigma.hpp
@@ -10,9 +10,11 @@ using namespace std;
  * Plugboard Class
  */
 class Plugboard {
+
   int config_file_integers[26];
   int config_int_count = 0;
   ifstream config_file;
+
 public:
   /*
    * Name: Plugboard constructor
@@ -21,25 +23,30 @@ public:
    * Output: initialised plugboard
    */
   int config(string config_file_path) {
-     size_t found = config_file_path.find_last_of("/\\");
+    size_t found = config_file_path.find_last_of("/\\");
     string config_file_name = config_file_path.substr(found+1);
     config_file.open(config_file_path);
+    int current_int;
+
+    // check file can be opened
     if (!config_file) {
       cerr << "ERROR_OPENING_CONFIGURATION_FILE" << endl;
       return ERROR_OPENING_CONFIGURATION_FILE;
     }
-    int current_int;
-    
+       
     while(config_file.good()) {
+      // check if we can fetch next int
       if (!(config_file >> current_int)) {
 	if (config_file.eof()) break;
 	cerr << "Non-numeric character in plugboard file " << config_file_name << endl;
 	return NON_NUMERIC_CHARACTER;
       }
+      // check if int is within range
       if (current_int < 0 || 25 < current_int) {
 	cerr << "Invalid Index used in plugboard file " << config_file_name << endl;
 	return INVALID_INDEX;
       }
+      // check mapping has not been used before
       for(int i; i < config_int_count; i++) {
 	if (current_int == config_file_integers[i]) {
 	  cerr << "Impossible plugboard configuration found in plugboard file" << config_file_name << endl;
@@ -49,6 +56,8 @@ public:
       config_file_integers[config_int_count] = current_int;
       config_int_count++;
     }
+
+    // check plugboard configurations are even and below 26
     if((config_int_count%2) || (config_int_count > 26)) {
       cerr << "Incorrect number of parameters in plugboard file " << config_file_name << endl;
       return INCORRECT_NUMBER_OF_PLUGBOARD_PARAMETERS;
@@ -67,6 +76,9 @@ public:
   void convert(char* input_char);
 };
 
+/*
+ * Rotor Class
+ */
 class Rotor {
   int rotate_notches[25];
   int rotator_notch_number = 0;
@@ -85,17 +97,19 @@ public:
    * Output: initialised rotor
    */
   int config(string config_file_path, string starting_pos_config_file_path, int rotor_pos) {
-     size_t found = config_file_path.find_last_of("/\\");
+    size_t found = config_file_path.find_last_of("/\\");
     string config_file_name = config_file_path.substr(found+1);
-     found = starting_pos_config_file_path.find_last_of("/\\");
-    string starting_pos_config_file_name = starting_pos_config_file_path.substr(found+1);
-    
+    found = starting_pos_config_file_path.find_last_of("/\\");
+    string starting_pos_config_file_name = starting_pos_config_file_path.substr(found+1);    
     config_file.open(config_file_path);
     starting_config.open(starting_pos_config_file_path);
-    if (!config_file || !starting_config) {
+
+    // Check config file opens
+    if (!config_file) {
       cerr <<  "Error occured whilst opening rotor file: " << config_file_name << endl;
       return ERROR_OPENING_CONFIGURATION_FILE;
     }
+    // Check starting position file opens
     if (!starting_config) {
       cerr << "Error occured whilst opening rotor position file: " << starting_pos_config_file_name << endl;
       return ERROR_OPENING_CONFIGURATION_FILE;
@@ -104,35 +118,37 @@ public:
     int current_int;
 
     while(config_file.good()) {
+       // check if we can fetch next int
       if (!(config_file >> current_int)) {
 	if (config_file.eof()) break;
         cerr << "Non-numeric character for mapping in rotor file " << config_file_name << endl;
         return NON_NUMERIC_CHARACTER;
       }
-
-     if (current_int < 0 || 25 < current_int) {
+      // check int is within range
+      if (current_int < 0 || 25 < current_int) {
         cerr << "Invalid Index used in rotor file " << config_file_name << endl;
         return INVALID_INDEX;
       }
-      
+      // check if next int is a notch or a rotor position
       if (config_int_count >= 26) {	
         rotate_notches[rotator_notch_number] = current_int;
 	rotator_notch_number++;
 	continue;
       }
-
+      // check that rotor position has not already been mapped to
       for(int i=0; i < config_int_count; i++) {
         if (current_int == rotor_mapping[i]) {
           cerr << "Invalid mapping of input " << config_int_count << " to output " << current_int << " (output " << current_int << " is already mapped to from input " << i << ") in rotor file " << config_file_name << endl;
           return INVALID_ROTOR_MAPPING;
         }
-	}
+      }
 
       rotor_mapping[config_int_count] = current_int;
       config_file_offsets[config_int_count] = current_int - config_int_count;
       config_int_count++;
     }
 
+    // check every rotor position has been mapped to
     if (config_int_count < 26) {
       cerr << "Not all inputs mapped in rotor file: " << config_file_name << endl;
       return INVALID_ROTOR_MAPPING;
@@ -140,7 +156,9 @@ public:
 
     int starting_pos;
     for (int i=0; i<=rotor_pos; i++) {
+      // check if we can fetch next in
       if(!(starting_config >> starting_pos)) {
+	// check whether we have reached the end of the file
 	if (starting_config.eof()) {
 	  cerr << "No starting position for rotor " << rotor_pos << " in rotor position file: " << starting_pos_config_file_name << endl;
 	  return NO_ROTOR_STARTING_POSITION;
@@ -151,27 +169,32 @@ public:
       }
     }
 
+    // check if we should rotate
     if (starting_pos > 0) {
       this->rotate(starting_pos);
-      }
+    }
 
 
     int next_pos;
+    // check if there is an invalid character at the end of the starting position file
     if(!(starting_config >> next_pos)) {
       if (starting_config.eof()) return 0;
       cerr << "Non-numeric character in rotor positions file " << starting_pos_config_file_name << endl;
-        return NON_NUMERIC_CHARACTER;
+      return NON_NUMERIC_CHARACTER;
     }
 
     return 0;
   }
 
-  void remap();
+  /*
+   *
+   * THE BELOW MUST BE DESCRIBED
+   *
+   */
+  void remap(); // make private
   void convert_forward(char* input_char);
   void convert_backward(char* input_char);
-
   void rotate(int by_positions=1);
-
   bool at_rotation_notch() {
     for(int current_notch=0; current_notch < rotator_notch_number; current_notch++) {
       if ((current_pos+1) == rotate_notches[current_notch]) return true;
